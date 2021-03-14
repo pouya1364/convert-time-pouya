63f8002356f5917fd62c18b0ba9af7707858bf8e
Pouya Amani <pouya1364@gmail.com>
2021-03-14 23:52:25 +0800

    44

--- public/asset/js/home.js
+++ public/asset/js/home.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function () {
+    // set date 
     var date =  $('#form_marsDate');
     date.datepicker({
         format: "yyyy-mm-dd",
@@ -7,14 +8,17 @@ $(document).ready(function () {
         autoclose: true
         
     });
+    // set today date
     date.datepicker('setDate', 'today');
 
+    // set time
     $('#form_marsTime').timepicker({
         format: 'hh-mm-ss',
         showMeridian: false,
         showSeconds: true,
     });
     
+    // on submit send date and time to api/convert
     $("#form_convert").submit(function(event){
         event.preventDefault();
       
@@ -23,17 +27,19 @@ $(document).ready(function () {
         var datetime = date+' '+time;
 
         $.ajax({
-            url : 'api/convert1',
+            url : 'api/convert',
             data: {datetime: datetime},
             type : "post",
             dataType: "json",
             success : function(response){
                 if (response.status ==200)
                 {
+                    // show mars and martian time
                     $("#mars_date").html(response.data.mars_sol_date);
                     $("#martin_date").html(response.data.martian_coordinated_time);
                    
                 } else {
+                    // show alert in case of bad response
                     $("#alert").toggle();
                     $("#alert").html(response.message);
 

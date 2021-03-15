779f593e5efdc5bddbce66ca69ed155268c3c938
Pouya Amani <pouya1364@gmail.com>
2021-03-15 12:56:21 +0800

    ss

--- tests/Controller/AppTest.php
+++ tests/Controller/AppTest.php
@@ -35,88 +35,85 @@ class AppTest extends TestCase
         $marsSolDate = $converter->getMarsSolDate();
         $martianTime = $converter->getMartianCoordinatedTime();
 
-        print_r($marsSolDate );
-        // print_r($martianTime );
-       
         $this->assertIsNumeric($marsSolDate);
         $this->assertIsString($martianTime);
         $this->assertEquals($marsSolDate, 52289.575513339696);
-        // $this->assertEquals($martianTime, '06:01:34');
+        $this->assertEquals($martianTime, '13:48:44');
     }
 
-    // public function test2()
-    // {
-    //     $request = Request::create(
-    //         '/api/convert',
-    //         'POST',
-    //         ['datetime' => '1985-09-01 19:10:25']
-    //     );
+    public function test2()
+    {
+        $request = Request::create(
+            '/api/convert',
+            'POST',
+            ['datetime' => '1985-09-01 19:10:25']
+        );
 
-    //     $reader = new DateValidator($request);
-    //     $converter = new MarsConverter($reader->getDateTime());
-    //     $marsSolDate = $converter->getMarsSolDate();
-    //     $martianTime = $converter->getMartianCoordinatedTime();
+        $reader = new DateValidator($request);
+        $converter = new MarsConverter($reader->getDateTime());
+        $marsSolDate = $converter->getMarsSolDate();
+        $martianTime = $converter->getMartianCoordinatedTime();
        
-    //     $this->assertIsNumeric($marsSolDate);
-    //     $this->assertIsString($martianTime);
-    //     $this->assertEquals($marsSolDate, 39696.652815764945);
-    //     $this->assertEquals($martianTime, '15:40:03');
-    // }
+        $this->assertIsNumeric($marsSolDate);
+        $this->assertIsString($martianTime);
+        $this->assertEquals($marsSolDate, 39696.93667868449);
+        $this->assertEquals($martianTime, '22:28:49');
+    }
 
-    // public function test3()
-    // {
-    //     $request = Request::create(
-    //         '/api/convert',
-    //         'POST',
-    //         ['datetime' => '1980-12-30 23:59:59']
-    //     );
+    public function test3()
+    {
+        $request = Request::create(
+            '/api/convert',
+            'POST',
+            ['datetime' => '1980-12-30 23:59:59']
+        );
 
-    //     $reader = new DateValidator($request);
-    //     $converter = new MarsConverter($reader->getDateTime());
-    //     $marsSolDate = $converter->getMarsSolDate();
-    //     $martianTime = $converter->getMartianCoordinatedTime();
+        $reader = new DateValidator($request);
+        $converter = new MarsConverter($reader->getDateTime());
+        $marsSolDate = $converter->getMarsSolDate();
+        $martianTime = $converter->getMartianCoordinatedTime();
        
-    //     $this->assertIsNumeric($marsSolDate);
-    //     $this->assertIsString($martianTime);
-    //     $this->assertEquals($marsSolDate, 38036.493754347255);
-    //     $this->assertEquals($martianTime, '11:51:00');
-    // }
+        $this->assertIsNumeric($marsSolDate);
+        $this->assertIsString($martianTime);
+        $this->assertEquals($marsSolDate, 38036.81816911277);
+        $this->assertEquals($martianTime, '19:38:09');
+    }
 
-    // public function test4()
-    // {
-    //     $request = Request::create(
-    //         '/api/convert',
-    //         'POST',
-    //         ['datetime' => '2121-03-14 12:55:55']
-    //     );
+    public function test4()
+    {
+        $request = Request::create(
+            '/api/convert',
+            'POST',
+            ['datetime' => '2121-03-14 12:55:55']
+        );
 
-    //     $reader = new DateValidator($request);
-    //     $converter = new MarsConverter($reader->getDateTime());
-    //     $marsSolDate = $converter->getMarsSolDate();
-    //     $martianTime = $converter->getMartianCoordinatedTime();
+        $reader = new DateValidator($request);
+        $converter = new MarsConverter($reader->getDateTime());
+        $marsSolDate = $converter->getMarsSolDate();
+        $martianTime = $converter->getMartianCoordinatedTime();
        
-    //     $this->assertIsNumeric($marsSolDate);
-    //     $this->assertIsString($martianTime);
-    //     $this->assertEquals($marsSolDate, 87873.9388497378);
-    //     $this->assertEquals($martianTime, '22:31:56');
-    // }
+        $this->assertIsNumeric($marsSolDate);
+        $this->assertIsString($martianTime);
+        $this->assertEquals($marsSolDate, 87874.26326450285);
+        $this->assertEquals($martianTime, '06:19:06');
+    }
 
-    // public function test5()
-    // {
-    //     $request = Request::create(
-    //         '/api/convert',
-    //         'POST',
-    //         ['datetime' => '2255-08-08 09:12:50']
-    //     );
+    public function test5()
+    {
+        $request = Request::create(
+            '/api/convert',
+            'POST',
+            ['datetime' => '2255-08-08 09:12:50']
+        );
 
-    //     $reader = new DateValidator($request);
-    //     $converter = new MarsConverter($reader->getDateTime());
-    //     $marsSolDate = $converter->getMarsSolDate();
-    //     $martianTime = $converter->getMartianCoordinatedTime();
+        $reader = new DateValidator($request);
+        $converter = new MarsConverter($reader->getDateTime());
+        $marsSolDate = $converter->getMarsSolDate();
+        $martianTime = $converter->getMartianCoordinatedTime();
        
-    //     $this->assertIsNumeric($marsSolDate);
-    //     $this->assertIsString($martianTime);
-    //     $this->assertEquals($marsSolDate, 135649.37732651317);
-    //     $this->assertEquals($martianTime, '09:03:21');
-    // }
+        $this->assertIsNumeric($marsSolDate);
+        $this->assertIsString($martianTime);
+        $this->assertEquals($marsSolDate, 135649.7017412787);
+        $this->assertEquals($martianTime, '16:50:30');
+    }
 }
